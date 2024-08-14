df16f80539daea0ef0c42bc15c4087a45ca21eeb
g0r0kh <gorokhovivan09@gmail.com>
2024-08-14 17:10:35 +0300

    add iterator

--- bcg.py
+++ bcg.py
@@ -4,6 +4,15 @@ import numpy as np
 # import xlsx
 data = pd.read_excel('source/agency_market_data_.xlsx',
                      sheet_name='sheet1')
+
+
+# Combine columns into a new column 'Combined'
+data['period'] = data['year'].astype(str) + data['quart'].astype(str)
+
+# iterable sorted list
+
+sort_list = sorted(set(data['period']))
+
 # split fot graph combine
 ATX = data[data['Метка']  == 'ATX']  # flt
 MKB = data[data['Метка']  != 'ATX']  # flt
@@ -13,19 +22,3 @@ MKB = data[data['Метка']  != 'ATX']  # flt
 
 
 
-# b.to_excel('result_ex1.xlsx',
-#               sheet_name='sheet_nm',
-#               index=True)
-
-# #
-# data['Terr']=np.where(data['Name'].str.endswith('Гинеколог'),
-#                       data['Name'].str.split(' ', n=1).str[0],
-#                       np.nan)
-#
-# data['Terr'] = data['Terr'].ffill()
-#
-# data = data[data['Name'].notnull()]
-#
-# data.to_excel('result_ex1.xlsx',
-#               sheet_name='sheet_nm',
-#               index=True)
