5601fd7e4fb3de740b1a16765985884fbe95a6a0
g0r0kh <gorokhovivan09@gmail.com>
2024-08-15 01:24:14 +0300

    add calculation dimensions 2

--- graph_base_one.py
+++ graph_base_one.py
@@ -9,11 +9,11 @@ ax = fig.add_subplot()
 fig.suptitle('BCG, Pharmacy market, ATX vs MKB, Ipsos: 2019-2022')
 
 # axes name
-# plt.gca().set(xlim=(0.0, data.x.quantile(0.95)), ylim=(data.y.quantile(0.05), data.y.quantile(0.95)),
-#               xlabel='market share, %', ylabel='evolution, %')
-
-plt.gca().set(xlim=(0.0, 30.0), ylim=(-30, 30),
+plt.gca().set(xlim=(0.0, data.x.quantile(0.95)), ylim=(data.y.quantile(0.05), data.y.quantile(0.95)),
               xlabel='market share, %', ylabel='evolution, %')
+
+# plt.gca().set(xlim=(0.0, 30.0), ylim=(-30, 30),
+#               xlabel='market share, %', ylabel='evolution, %')
 
 ax.scatter(data.x, data.y, s=data.x*5, c=data.color, alpha=0.5)
 
