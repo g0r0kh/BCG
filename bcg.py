90b24bdee5c30fc60eb350aabc0db5756bfd854b
g0r0kh <gorokhovivan09@gmail.com>
2024-08-15 23:56:12 +0300

    add clear

--- bcg.py
+++ bcg.py
@@ -9,13 +9,7 @@ data = pd.read_excel('source/agency_market_data_.xlsx',
 # Combine columns into a new column 'Combined'
 data['period'] = data['year'].astype(str) + data['quart'].astype(str)
 data['color'] = np.where(data.Метка == 'ATX', 'g', 'b')
-# iterable sorted list
-# df['d'] = np.where(df.c > df.b, 'Type a', 'Type b')
-# sort_list = sorted(set(data['period']))
 
-# split fot graph combine
-# ATX = data[data['Метка']  == 'ATX']  # flt
-# MKB = data[data['Метка']  != 'ATX']  # flt
 
 
 
