05e08bfd665a8cf15ee15018a97b5203c399e951
g0r0kh <gorokhovivan09@gmail.com>
2024-08-16 13:31:02 +0300

    add labels combine expose

--- graph_anim.py
+++ graph_anim.py
@@ -23,15 +23,14 @@ for i in sort_list:
 
     # data point params
     scatter = plt.scatter(a.x, a.y, s=a.x * 10, c=a.color, alpha=0.5)
-    # legend
-    # plt.legend(handles=scatter.legend_elements()[0], labels=classes)
 
-    # # label
-    # # label pochti
+    # legend is disable cause one dimension
+
+    # # labels
     a = a.reset_index()
     for ii in range(len(a.x)):
         if a.name_short[ii] != 'empty':
-            plt.annotate(a.name_short[ii], (a.x[ii], a.y[ii]))
+            plt.annotate(a.Метка[ii] + ':' + a.name_short[ii], (a.x[ii], a.y[ii]))
         else:
             continue
 
@@ -40,35 +39,6 @@ for i in sort_list:
     plt.gcf().canvas.flush_events()
 
     time.sleep(0.99)
-
-
-   #
-   # # # label pochti
-   #  for ii in range(len(a.x)):
-   #      if data.name_short[ii] != 'empty':
-   #          plt.annotate(data.name_short[ii], (data.x[ii], data.y[ii]))
-   #      else:
-   #          continue
-
-
-
-
-# # label
-# for ii in range(len(data.x)):
-#     if data.name_short[ii] != 'empty':
-#         plt.annotate(data.name_short[ii], (data.x[ii], data.y[ii]))
-#     else:
-#         continue
-# # label sample
-# legend=[str(year) for year in df['year'].unique()]
-# plt.title('Battery Capicity kwh')
-# result = plt.scatter('Approx_Release_price_order_in_K$','Battery_Capacity_kWh',data=df,c='year',label='Class 1')
-# plt.ylabel('kwh')
-# plt.xlabel('K$')
-# plt.legend(handles=result.legend_elements()[0],
-#        labels=legend,
-#        title="Year")
-# print('The higher priced evs have more battery capacity')
 
 
 plt.ioff()
