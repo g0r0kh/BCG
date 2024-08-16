7b50d3e8a79d64cfc28bb139fd22395c550bb5ef
g0r0kh <gorokhovivan09@gmail.com>
2024-08-16 12:44:39 +0300

    add labels expose

--- graph_anim.py
+++ graph_anim.py
@@ -1,9 +1,10 @@
+
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from bcg import data
+from bcg import data, data_cut
 
-sort_list = sorted(set(data['period']))
+sort_list = sorted(set(data_cut['period']))
 
 
 plt.ion()
@@ -17,17 +18,38 @@ for i in sort_list:
     plt.suptitle('BCG, Pharmacy market, ATC vs ICD - 10, Ipsos: 2019-2022')
     # axes name axes limits
     plt.gca().set(xlim=(0.0, a.x.quantile(0.95)), ylim=(a.y.quantile(0.05), a.y.quantile(0.75)),
-                    xlabel='market share, %', ylabel='evolution, %')
+                  xlabel='market share, %', ylabel='evolution, %')
+
 
     # data point params
     scatter = plt.scatter(a.x, a.y, s=a.x * 10, c=a.color, alpha=0.5)
     # legend
     # plt.legend(handles=scatter.legend_elements()[0], labels=classes)
 
+    # # label
+    # # label pochti
+    a = a.reset_index()
+    for ii in range(len(a.x)):
+        if a.name_short[ii] != 'empty':
+            plt.annotate(a.name_short[ii], (a.x[ii], a.y[ii]))
+        else:
+            continue
+
+
     # plt.draw()
     plt.gcf().canvas.flush_events()
 
-    time.sleep(0.19)
+    time.sleep(0.99)
+
+
+   #
+   # # # label pochti
+   #  for ii in range(len(a.x)):
+   #      if data.name_short[ii] != 'empty':
+   #          plt.annotate(data.name_short[ii], (data.x[ii], data.y[ii]))
+   #      else:
+   #          continue
+
 
 
 
